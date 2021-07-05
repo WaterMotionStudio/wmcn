4bb0a15ba8c21d4c92db19e388a1acf631b157ba
WaterMotionStudio <86828048+WaterMotionStudio@users.noreply.github.com>
2021-07-05 16:16:24 +0800

    Create intranet.js

---
+++ intranet.js
@@ -0,0 +1,18 @@
+     function showPass(form){
+        
+       var user=form.username.value;
+        var pass=form.pwd.value;
+        
+        var user1="admin"
+        var pass1="wearyisthebestemote"
+        
+        if(user==user1 && pass==pass1){
+         
+          window.open="elements.html";
+        }
+        else
+          window.open="intranet.html";
+        
+      }
+      
+      
